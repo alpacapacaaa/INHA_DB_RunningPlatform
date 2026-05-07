b92a0549c787b27571764f368bde93006fdce1fb
yeongjun25 <ss91530441@gmail.com>
2026-05-07 22:27:13 +0900

    chore: 백엔드 프로젝트 초기 세팅

---
+++ backend/src/test/java/com/shaperun/shaperun/ShaperunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaperun.shaperun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShaperunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
