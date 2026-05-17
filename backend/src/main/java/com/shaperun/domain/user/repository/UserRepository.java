c46de2610eac7c9e8c9e9cb14de9ba6953104309
yeongjun25 <ss91530441@gmail.com>
2026-05-17 22:02:31 +0900

    fix: Optional import 미사용으로 제거

--- backend/src/main/java/com/shaperun/domain/user/repository/UserRepository.java
+++ backend/src/main/java/com/shaperun/domain/user/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.shaperun.domain.user.repository;
 
 import com.shaperun.domain.user.entity.User;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, String> {
