612751a08392fd637cab15e7d2f6b0f0d2c1eb70
yeongjun25 <ss91530441@gmail.com>
2026-05-17 20:39:35 +0900

    feat: User 도메인 서비스/컨트롤러/DTO 구현 및 프로필 응답 필드 정리

--- backend/src/main/java/com/shaperun/domain/user/repository/UserRepository.java
+++ backend/src/main/java/com/shaperun/domain/user/repository/UserRepository.java
@@ -1,12 +1,9 @@
 package com.shaperun.domain.user.repository;
 
 import com.shaperun.domain.user.entity.User;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, String> {
-
-    Optional<User> findByEmail(String email);
 
     boolean existsByUserId(String userId);
 
