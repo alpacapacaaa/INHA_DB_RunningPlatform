6cadc976ae585c2f410ba61f9b6bc2e049511537
yeongjun25 <ss91530441@gmail.com>
2026-05-17 17:24:28 +0900

    feat: User 엔티티 및 레포지토리 구현 - JwtAuthenticationFilter 컴파일 에러 수정)

--- backend/src/main/java/com/shaperun/domain/user/repository/UserRepository.java
+++ backend/src/main/java/com/shaperun/domain/user/repository/UserRepository.java
@@ -1,7 +1,15 @@
 package com.shaperun.domain.user.repository;
 
 import com.shaperun.domain.user.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByUserId(String userId);
+
+    boolean existsByEmail(String email);
 }
+
